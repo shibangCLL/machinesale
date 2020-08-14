1c0984b8187bd61c4913003e97d4411550af610a
shibangCLL <869241891@qq.com>
2020-08-14 22:52:51 +0800

    Django制作企业官网-03.创建APP

--- machinesale/settings.py
+++ machinesale/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products' #注册products APP
 ]
 
 MIDDLEWARE = [
---
+++ products/urls.py
@@ -0,0 +1,9 @@
+# 引入path
+from django.urls import path
+
+# 正在部署的应用的名称
+app_name = 'products'
+
+urlpatterns = [
+    # 目前还没有urls
+]
